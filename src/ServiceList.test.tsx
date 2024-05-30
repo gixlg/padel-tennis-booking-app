d45925fa99148f78ab7a88d9e8035875c0509b0d
Luca Giuberti <gix_lg@yahoo.com>
2024-05-30 18:35:56 +0200

    Green - ServiceList - added checkboxes

--- src/ServiceList.test.tsx
+++ src/ServiceList.test.tsx
@@ -1,13 +1,47 @@
 import { render, screen} from "@testing-library/react";
 
+type Service =  {
+    name: string,
+    id: string,
+    value: number,
+    description: string,
+};
 
-function ServiceList() {
-    return null;
+type Services = Service[];
+
+function ServiceList({services}: {services: Services}) {
+    return <>
+        {
+            services.map(s =>
+                <input
+                    key={s.name}
+                    type="checkbox"
+                    name={s.name}
+                    value={s.value}
+                />
+            )
+        }
+    </>
 }
 
 describe("Service List", () => {
     it("Should render multiple checkbox", () => {
-        render(<ServiceList/>)
+        const services = [
+            {
+                name: "Field",
+                id: "field",
+                value: 30,
+                description: "An Amazing Field",
+            },
+            {
+                name: "Shower",
+                id: "shower",
+                value: 5,
+                description: "An good Shower",
+            }
+        ]
+
+        render(<ServiceList services={services}/>)
 
         expect(screen.getAllByRole("checkbox")).toHaveLength(2);
     })
