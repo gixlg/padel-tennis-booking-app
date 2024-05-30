982726dd81df5399c6f6a159bc9619cea26526b2
Luca Giuberti <gix_lg@yahoo.com>
2024-05-30 23:31:01 +0200

    Refactor - ServiceList - extracted component

--- src/ServiceList.test.tsx
+++ src/ServiceList.test.tsx
@@ -1,52 +1,6 @@
 import {fireEvent, render, screen, waitFor} from "@testing-library/react";
-import {ChangeEvent, useEffect, useState} from "react";
-import {fetchServices} from "./DomainLogic";
 import * as domainLogic from "./DomainLogic";
-
-type Service =  {
-    name: string,
-    id: string,
-    value: number,
-    description: string,
-};
-
-type Services = Service[];
-
-function ServiceList({sport}: { sport: string }) {
-    const [total, setTotal] = useState(0)
-    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const input: HTMLInputElement = event.target;
-        setTotal((current) => input.checked ? current + Number(input.value) : current - Number(input.value))
-
-    };
-
-    const [services, setServices] = useState<Services>([]);
-
-    useEffect(() => {
-        fetchServices(sport)
-            .then(setServices)
-    }, []);
-
-    return <>
-        {
-            services.map(s =>
-                <div key={s.name}>
-                    <p>{s.description}</p>
-                    <label>
-                        <input
-                            type="checkbox"
-                            name={s.name}
-                            value={s.value}
-                            onChange={onChange}
-                        />
-                        {s.name}
-                    </label>
-                </div>
-            )
-        }
-        <p>Total is: {total}</p>
-    </>
-}
+import {ServiceList} from "./ServiceList";
 
 describe("Service List", () => {
     beforeEach(()=>{
